b0ec754dc8a659a824882593746811033ac05345
Artem <inste@icloud.com>
2019-05-21 12:04:32 +0300

    Initial Commit

---
+++ ParallaxEffect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParallaxEffect
+//
+//  Created by Art Karma on 5/21/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
